7e54c812260c428ca2f0c4f55766a3c8a324a63f
alonginwind <sunzl3721@gmail.com>
2026-06-29 10:21:45 +0800

    修复添加删除书籍时侧边栏统计数据未实时更新

--- app/src/pages/book/_bookid.vue
+++ app/src/pages/book/_bookid.vue
@@ -2515,6 +2515,7 @@ export default {
                 method: "POST",
             }).then((rsp) => {
                 if (rsp.err === "ok") {
+                    this.$store.commit("refresh_sys");
                     this.$alert("success", this.$t('message.deleteSuccess'));
                     this.$router.push("/");
                 } else {
@@ -2854,6 +2855,7 @@ export default {
                     if (rsp.err != "ok") {
                         this.$alert("error", rsp.msg);
                     } else {
+                        this.$store.commit("refresh_sys");
                         // 如果勾选了继续添加，则跳转时携带参数，否则直接跳转
                         if (this.continueAdding) {
                             this.$router.push(`/book/${rsp.book_id}?continue_adding=true`);
